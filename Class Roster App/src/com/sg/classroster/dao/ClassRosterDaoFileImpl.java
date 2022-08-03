b5195e74e53242b3fdeaff913d3d5535c9695b8a
owyand <oliverwyand@gmail.com>
2022-08-02 23:27:09 -0400

    commit

--- Class Roster App/src/com/sg/classroster/dao/ClassRosterDaoFileImpl.java
+++ Class Roster App/src/com/sg/classroster/dao/ClassRosterDaoFileImpl.java
@@ -1,14 +1,18 @@
 package com.sg.classroster.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.sg.classroster.dto.Student;
 
 public class ClassRosterDaoFileImpl implements ClassRosterDao {
+	
+	final Map<String, Student> students = new HashMap<>();
 
 	@Override
 	public Student addStudent(String studentID, Student student) {
-		throw new UnsupportedOperationException("Not supported yet");
+		return students.put(studentID, student);
 	}
 
 	@Override
--- Class Roster App/src/com/sg/classroster/ui/ClassRosterView.java
+++ Class Roster App/src/com/sg/classroster/ui/ClassRosterView.java
@@ -1,5 +1,7 @@
 package com.sg.classroster.ui;
 
+import com.sg.classroster.dto.Student;
+
 public class ClassRosterView {
 	
 	private UserIO io = new UserIOConsoleImpl();
@@ -14,5 +16,25 @@ public class ClassRosterView {
 		
 		//this calls a method from UserIOConsoleImpl
 		return io.readInt("Please select from the above choices", 1, 5);
+	}
+	
+	//gets info from user to create and store a new student object
+	public Student getNewStudentInfo() {
+		Student currentStudent = new Student(io.readString("Please enter Student ID"));
+		
+		currentStudent.setFirstName(io.readString("Please enter First Name"));
+		currentStudent.setLastName(io.readString("Please enter Last Name"));
+		currentStudent.setCohort(io.readString("Please enter Cohort"));
+		
+		return currentStudent;
+	}
+	
+	//banner
+	public void displayCreateStudentBanner() {
+		io.print("=== Create Student ===");
+	}
+	
+	public void displayCreateSuccesssBanner() {
+		io.readString("Student successfully create. Please hit continue");
 	}
 }
